1c788a890f10139c9a676cb2d26a91dd819e0e7f
antant <ant.iv.ge@gmail.com>
2023-03-28 14:28:48 +0300

    added validations and using userAddress from token

--- src/controllers/taskVerifier.controller.ts
+++ src/controllers/taskVerifier.controller.ts
@@ -1,24 +1,32 @@
 import { LoggerService } from "../services/logger.service";
 import { injectable } from "inversify";
 import { Response } from "express";
-import { verifyTransaction, verifyQuizTask, verifyJoinDiscordTask } from "../services/taskVerifiers";
+import {
+  verifyTransaction,
+  verifyQuizTask,
+  verifyJoinDiscordTask,
+} from "../services/taskVerifiers";
 
 @injectable()
 export class TaskVerifierController {
-  constructor(private loggerService: LoggerService) { }
+  constructor(private loggerService: LoggerService) {}
 
   public verifyTransactionTask = async (req: any, res: Response) => {
     try {
-
       const pluginAddress: string = req.body.onboardingPluginAddress;
       const taskAddress: string = req.body.taskAddress;
       const taskId: number = req.body.taskId;
       const submitter: string = req.body.submitter;
-      const finalizedResult = await verifyTransaction(pluginAddress, taskAddress, taskId, submitter, 'mumbai');
+      const finalizedResult = await verifyTransaction(
+        pluginAddress,
+        taskAddress,
+        taskId,
+        submitter,
+        "mumbai"
+      );
       if (finalizedResult.isFinalized)
         return res.status(200).send(finalizedResult);
-      else
-        return res.status(400).send(finalizedResult);
+      else return res.status(400).send(finalizedResult);
     } catch (err) {
       this.loggerService.error(err);
       return res
@@ -27,20 +35,37 @@ export class TaskVerifierController {
     }
   };
 
-
   public verifyQuizTask = async (req: any, res: Response) => {
     try {
-
+      const submitter: string = req.user.address;
       const onboardingPluginAddress: string = req.body.onboardingPluginAddress;
+      if (onboardingPluginAddress) {
+        return res
+          .status(400)
+          .send({ error: "onboardingPluginAddress not provided." });
+      }
       const taskAddress: string = req.body.taskAddress;
+      if (taskAddress) {
+        return res.status(400).send({ error: "taskAddress not provided." });
+      }
       const taskId: number = req.body.taskId;
-      const submitter: string = req.body.submitter;
+      if (taskId) {
+        return res.status(400).send({ error: "taskId not provided." });
+      }
       const answers: string[] = req.body.answers;
-      const finalizedResult = await verifyQuizTask(onboardingPluginAddress, taskAddress, taskId, submitter, answers);
+      if (answers) {
+        return res.status(400).send({ error: "answers not provided." });
+      }
+      const finalizedResult = await verifyQuizTask(
+        onboardingPluginAddress,
+        taskAddress,
+        taskId,
+        submitter,
+        answers
+      );
       if (finalizedResult.isFinalized)
         return res.status(200).send(finalizedResult);
-      else
-        return res.status(400).send(finalizedResult);
+      else return res.status(400).send(finalizedResult);
     } catch (err) {
       this.loggerService.error(err);
       return res
@@ -51,16 +76,35 @@ export class TaskVerifierController {
 
   public verifyDiscordJoinTask = async (req: any, res: Response) => {
     try {
+      const submitter: string = req.user.address;
       const onboardingPluginAddress: string = req.body.onboardingPluginAddress;
+      if (onboardingPluginAddress) {
+        return res
+          .status(400)
+          .send({ error: "onboardingPluginAddress not provided." });
+      }
       const taskAddress: string = req.body.taskAddress;
+      if (taskAddress) {
+        return res.status(400).send({ error: "taskAddress not provided." });
+      }
       const taskId: number = req.body.taskId;
-      const submitter: string = req.body.submitter;
+      if (taskId) {
+        return res.status(400).send({ error: "taskId not provided." });
+      }
       const bearerToken: string = req.body.bearerToken;
-      const finalizedResult = await verifyJoinDiscordTask(onboardingPluginAddress, taskAddress, taskId, submitter, bearerToken);
+      if (bearerToken) {
+        return res.status(400).send({ error: "bearerToken not provided." });
+      }
+      const finalizedResult = await verifyJoinDiscordTask(
+        onboardingPluginAddress,
+        taskAddress,
+        taskId,
+        submitter,
+        bearerToken
+      );
       if (finalizedResult.isFinalized)
         return res.status(200).send(finalizedResult);
-      else
-        return res.status(400).send(finalizedResult);
+      else return res.status(400).send(finalizedResult);
     } catch (err) {
       this.loggerService.error(err);
       return res

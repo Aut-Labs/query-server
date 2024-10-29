4ce0bd04d054023e8ff817b9a553056d457ca882
Your Name <you@example.com>
2024-10-29 14:48:48 +0200

    mongoose connection

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const logger = container.get<LoggerService>(LoggerService);
 application.app.listen(PORT, async () => {
   try {
     try {
-      // await connect(process.env.MONGODB_CONNECTION_STRING);
+      await connect(process.env.MONGODB_CONNECTION_STRING);
       console.log("Connected to mongoose!");
     } catch (error) {
       console.log(error, "error");
